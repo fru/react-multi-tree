8e95af68d0fc1ce0a9302a2047138379e99abbd1
Florian Rüberg <florian.rueberg@gmail.com>
2018-04-12 13:16:13 +0200

    Cleanup webpack config

--- config/storybook/webpack.config.js
+++ config/storybook/webpack.config.js
@@ -1,21 +1,7 @@
-/* global __dirname, require, module*/
-
 const webpack = require('webpack');
 const path = require('path');
 
-/*const env = require('yargs').argv.env || {};
-const folder = env.folder || 'docs';
-const isBuild = env.env === 'build';
-const isLibrary = folder !== 'docs';*/
-
 const config = {
-  /*entry: __dirname + '/' + folder + '/index.js',
-  devtool: 'source-map',
-  output: {
-    path: __dirname + '/' + folder + '/dist',
-    filename: 'index.min.js',
-    publicPath: 'anyform/dist'
-  },*/
   module: {
     rules: [
       {
@@ -26,20 +12,9 @@ const config = {
       {
         test: /\.scss$/,
         loaders: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.png$/,
-        loader: 'url-loader',
       }
     ]
-  },
-  /*devServer: {
-    port: 3001,
-    hot: true,
-    historyApiFallback: {
-      index: './docs/index.html'
-    }
-  }*/
+  }
 };
 
 module.exports = config;
